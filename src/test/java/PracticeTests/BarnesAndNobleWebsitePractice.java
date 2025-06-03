94f652551b779730c1d3768ac9223636854a6761
Mihaela Budeanu <mihaela.budeanu@endava.com>
2025-06-03 16:35:04 +0300

    - created a new package and moved 3 tests

--- src/test/java/PracticeTests/BarnesAndNobleWebsitePractice.java
+++ src/test/java/PracticeTests/BarnesAndNobleWebsitePractice.java
@@ -1,13 +1,9 @@
-package Tests;
+package PracticeTests;
 
-import java.io.File;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -15,7 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BarnesAndNobleWebsitePractice {
--- src/test/java/PracticeTests/MacysWebsitePracticeTest.java
+++ src/test/java/PracticeTests/MacysWebsitePracticeTest.java
@@ -1,8 +1,7 @@
-package Tests;
+package PracticeTests;
 
 import java.time.Duration;
 import org.openqa.selenium.By;
-import org.openqa.selenium.By.ByLinkText;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
