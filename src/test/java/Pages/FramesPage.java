a81f9f828287c9a4a355f3f9dc9a3185f085f6e3
Mihaela Budeanu <mihaela.budeanu@endava.com>
2025-06-24 11:20:15 +0300

    - finished session 20 - refactored "AlertTestPage" and "FramesPage" classes by adding logs to the extentReports - finished report utility and improved logs

--- src/test/java/Pages/FramesPage.java
+++ src/test/java/Pages/FramesPage.java
@@ -5,17 +5,18 @@ import static org.testng.Assert.assertNotNull;
 import HelperMethods.ElementsMethods;
 import HelperMethods.FramesMethods;
 import HelperMethods.JavascriptMethods;
+import logger.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class FramesPage extends CommonPage{
+public class FramesPage extends CommonPage {
 
-    @FindBy(id="frame1")
+    @FindBy(id = "frame1")
     private WebElement frame1;
-    @FindBy(id="frame2")
+    @FindBy(id = "frame2")
     private WebElement frame2;
-    @FindBy(id="sampleHeading")
+    @FindBy(id = "sampleHeading")
     private WebElement sampleHeadingElement;
 
     public FramesPage(WebDriver driver) {
@@ -24,16 +25,21 @@ public class FramesPage extends CommonPage{
 
     public void switchToFrame1() {
         framesMethods.switchToFrame(frame1);
+        LoggerUtility.infoLog("The frame is switched to the frame 1");
     }
+
     public void switchToFrame2() {
         framesMethods.switchToFrame(frame2);
+        LoggerUtility.infoLog("The frame is switched to the frame 2");
     }
+
     public void verifySampleHeadingText(String expectedText) {
         assertNotNull(sampleHeadingElement, "Elementul nu a fost gasit in frame-ul 1");
+        LoggerUtility.infoLog("The sample heading is: " + sampleHeadingElement.getText());
         //verificam ca textul din frame este corect
         String sampleHeading = sampleHeadingElement.getText();
         assert sampleHeading.equals(expectedText) : "Textul din frame-ul 1 nu este corect. Expected: " + expectedText + ", Actual: " + sampleHeading;
-
+        LoggerUtility.infoLog("The sample heading is: " + sampleHeading);
     }
 
 }
